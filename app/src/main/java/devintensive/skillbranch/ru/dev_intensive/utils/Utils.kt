c7380fb266406072a4594358ec907a9f38862ef1
Влад Акулинин <vadson40@gmail.ru>
2019-07-10 18:01:01 +0300

    work

--- app/src/main/java/devintensive/skillbranch/ru/dev_intensive/utils/Utils.kt
+++ app/src/main/java/devintensive/skillbranch/ru/dev_intensive/utils/Utils.kt
@@ -3,8 +3,8 @@ package devintensive.skillbranch.ru.dev_intensive.utils
 object Utils {
 
     //разбиение одной строки на несколько
-    fun parseFullName(fullName:String?):Pair<String?, String?>{
-        val parts : List<String>? = fullName?.split(" ")
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        val parts: List<String>? = fullName?.split(" ")
 
         var firstName = parts?.getOrNull(0)
         var lastName = parts?.getOrNull(1)
@@ -15,62 +15,61 @@ object Utils {
         return firstName to lastName
     }
 
-    fun transliteration(payload: String, divider:String = " "): String {
+    fun transliteration(payload: String, divider: String = " "): String {
+        var newPayload: String = payload
 
-            payload.replace("а", "a")
-            payload.replace("б", "b")
-            payload.replace("в", "v")
-            payload.replace("г", "g")
-            payload.replace("д", "d")
-            payload.replace("е", "e")
-            payload.replace("ё", "e")
-            payload.replace("ж", "zh")
-            payload.replace("з", "z")
-            payload.replace("и", "i")
-            payload.replace("й", "i")
-            payload.replace("к", "k")
-            payload.replace("л", "l")
-            payload.replace("м", "m")
-            payload.replace("н", "n")
-            payload.replace("о", "o")
-            payload.replace("п", "p")
-            payload.replace("р", "r")
-            payload.replace("с", "s")
-            payload.replace("т", "t")
-            payload.replace("у", "u")
-            payload.replace("ф", "f")
-            payload.replace("х", "h")
-            payload.replace("ц", "c")
-            payload.replace("ч", "ch")
-            payload.replace("ш", "sh")
-            payload.replace("щ", "sh")
-            payload.replace("ъ", "")
-            payload.replace("ы", "i")
-            payload.replace("ь", "")
-            payload.replace("э", "e")
-            payload.replace("ю", "yu")
-            payload.replace("я", "ya")
+        newPayload = newPayload.replace("а", "a")
+        newPayload = newPayload.replace("б", "b")
+        newPayload = newPayload.replace("в", "v")
+        newPayload = newPayload.replace("г", "g")
+        newPayload = newPayload.replace("д", "d")
+        newPayload = newPayload.replace("е", "e")
+        newPayload = newPayload.replace("ё", "e")
+        newPayload = newPayload.replace("ж", "zh")
+        newPayload = newPayload.replace("з", "z")
+        newPayload = newPayload.replace("и", "i")
+        newPayload = newPayload.replace("й", "i")
+        newPayload = newPayload.replace("к", "k")
+        newPayload = newPayload.replace("л", "l")
+        newPayload = newPayload.replace("м", "m")
+        newPayload = newPayload.replace("н", "n")
+        newPayload = newPayload.replace("о", "o")
+        newPayload = newPayload.replace("п", "p")
+        newPayload = newPayload.replace("р", "r")
+        newPayload = newPayload.replace("с", "s")
+        newPayload = newPayload.replace("т", "t")
+        newPayload = newPayload.replace("у", "u")
+        newPayload = newPayload.replace("ф", "f")
+        newPayload = newPayload.replace("х", "h")
+        newPayload = newPayload.replace("ц", "c")
+        newPayload = newPayload.replace("ч", "ch")
+        newPayload = newPayload.replace("ш", "sh")
+        newPayload = newPayload.replace("щ", "sh")
+        newPayload = newPayload.replace("ъ", "")
+        newPayload = newPayload.replace("ы", "i")
+        newPayload = newPayload.replace("ь", "")
+        newPayload = newPayload.replace("э", "e")
+        newPayload = newPayload.replace("ю", "yu")
+        newPayload = newPayload.replace("я", "ya")
 
-        return payload
+        return newPayload
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
 
-        var oneFirstName:String ?= ""
-        var oneLastName:String ?= ""
-        var inicial:String ?= ""
+        var oneFirstName: String? = ""
+        var oneLastName: String? = ""
+        var inicial: String? = ""
 
-        if(!firstName.isNullOrBlank() && firstName != "null"){
+        if (!firstName.isNullOrBlank() && firstName != "null") {
             oneFirstName = firstName.get(0).toString()//replaseInitials(firstName.get(0).toString())
-        }
-        else oneFirstName = ""
+        } else oneFirstName = ""
 
-        if(!lastName.isNullOrBlank() && lastName != "null"){
+        if (!lastName.isNullOrBlank() && lastName != "null") {
             oneLastName = lastName.get(0).toString()//replaseInitials(lastName.get(0).toString())
-        }
-        else oneLastName = ""
+        } else oneLastName = ""
 
-        if(firstName.isNullOrBlank() && lastName.isNullOrBlank())
+        if (firstName.isNullOrBlank() && lastName.isNullOrBlank())
             inicial = null
         else
             inicial = oneFirstName + oneLastName
@@ -78,9 +77,9 @@ object Utils {
         return inicial
     }
 
-    fun replaseInitials(simbol:String):String{
-        var simbolExit : String ?= ""
-        when(simbol) {
+    fun replaseInitials(simbol: String): String {
+        var simbolExit: String? = ""
+        when (simbol) {
             "а" -> simbolExit = "a"
             "б" -> simbolExit = "b"
             "в" -> simbolExit = "v"
